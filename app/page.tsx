39a7ff2001dc39f643764651dd0b7e3c6bd3e581
Mehran <realmehranasadi@gmail.com>
2023-06-16 12:24:56 +0330

    make carcard

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { CustomFilter, Hero, SearchBar } from "@/components";
+import { CustomFilter, Hero, SearchBar, CarCard } from "@/components";
 import { fetchCars } from "@/utils";
 
 export default async function Home() {
@@ -28,7 +28,13 @@ export default async function Home() {
           </div>
         </div>
         {!idDataEmpty ? (
-          <section>WE HAVE CARS</section>
+          <section>
+            <div className="home__cars-wrapper">
+              {allCars.map((car) => (
+                <CarCard car={car} />
+              ))}
+            </div>
+          </section>
         ) : (
           <div className="home__error-container">
             <h2 className="text-black text-xl font-bold">Oops , no results</h2>
--- components/index.ts
+++ components/index.ts
@@ -5,6 +5,7 @@ import Footer from "./Footer";
 import SearchBar from "./SearchBar";
 import CustomFilter from "./CustomFilter";
 import SearchManuFacture from "./SearchManuFacture";
+import CarCard from "./CarCard";
 
 export {
   Hero,
@@ -14,4 +15,5 @@ export {
   SearchBar,
   CustomFilter,
   SearchManuFacture,
+  CarCard,
 };
