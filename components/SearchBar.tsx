9a85acd7c9ba495c6bff308ade844bf82f8ed7bb
Mehran <realmehranasadi@gmail.com>
2023-06-18 18:33:03 +0330

    add searchbar model

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -2,11 +2,25 @@
 
 import React from "react";
 import { SearchManuFacture } from "./";
+import Image from "next/image";
 
 import { useState } from "react";
 
+const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
+  <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
+    <Image
+      src="/magnifying-glass.svg"
+      alt="magnifying-glass"
+      width={40}
+      height={40}
+      className="object-contain"
+    />
+  </button>
+);
+
 function SearchBar() {
   const [manuFacturer, setManuFacturer] = useState("");
+  const [model, setModel] = useState("");
   const handelSearch = () => {};
   return (
     <form className="searchbar" onSubmit={handelSearch}>
@@ -15,7 +29,27 @@ function SearchBar() {
           manuFacturer={manuFacturer}
           setManuFacturer={setManuFacturer}
         />
+        <SearchButton otherClasses="sm:hidden" />
       </div>
+      <div className="searchbar__item">
+        <Image
+          src="/model-icon.png"
+          width={25}
+          height={25}
+          className="absolute w-[20px] h-[20px] ml-4"
+          alt="car model"
+        />
+        <input
+          type="text"
+          name="model"
+          value={model}
+          onChange={(e) => setModel(e.target.value)}
+          placeholder="Tiguan"
+          className="searchbar__input"
+        />
+        <SearchButton otherClasses="sm:hidden" />
+      </div>
+      <SearchButton otherClasses="max-sm:hidden" />
     </form>
   );
 }
